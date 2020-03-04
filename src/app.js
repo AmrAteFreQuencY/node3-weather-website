163db107a6f37644e0343b4f396f4cf06afdd171
AMR ATEF <you@example.com>
2020-03-04 18:03:17 +0200

    Setup app for heroku

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const publicDirectoryPath = path.join(__dirname, "../public");
 const viewsPath = path.join(__dirname, "../templates/views");
 const partialsPath = path.join(__dirname, "../templates/partials");
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.set("view engine", "hbs");
 app.set("views", viewsPath);
@@ -94,6 +95,6 @@ app.get("*", (req, res) => {
     errorMessage: "Page Not Found"
   });
 });
-app.listen(3000, () => {
-  console.log("server is up on port 3000.");
+app.listen(port, () => {
+  console.log("server is up on port " + port);
 });
